64666e610d9ce459a90ae8f44de43cd8574a1269
chunChu <86117554+DoanTChung-HE176077@users.noreply.github.com>
2023-12-04 15:52:14 +0700

    add mongo to nodejs
    - update gitignore
    - connect mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://chun:Chundb123@mern-book-store.rcdelem.mongodb.net/books-collection?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,7 +9,23 @@ app.get("/", (req, res) => {
   return res.status(234).send("Welcome");
 });
 
-app.listen(PORT, () => {
-  console.log("===== CHECK STATUS: PORT =====");
-  console.log(`GOOD: App is listening to PORT: ${PORT}`);
-});
+//move this to mongoose then, if want app run only
+//if db connection is successful
+// app.listen(PORT, () => {
+//   console.log("===== CHECK STATUS: PORT =====");
+//   console.log(`GOOD: App is listening to PORT: ${PORT}`);
+// });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("===== CHECK STATUS: APP CONNECT TO DB =====");
+    console.log("GOOD: App connect to database");
+    app.listen(PORT, () => {
+      console.log("===== CHECK STATUS: PORT =====");
+      console.log(`GOOD: App is listening to PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

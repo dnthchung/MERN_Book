e0f18bd589d09f7487b36857b43ae0562d6f3cdd
chunChu <86117554+DoanTChung-HE176077@users.noreply.github.com>
2023-12-11 10:35:39 +0700

    Get All Books with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { request, response } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import { Book } from "./model/bookModel.js";
 import { PORT, mongoDBURL } from "./config.js";
@@ -37,6 +37,11 @@ app.post("/books", async (req, res) => {
 app.get("/books", async (request, response) => {
   try {
     const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });

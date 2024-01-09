6e55ee3c7c901154d1c0985d8e5e4a49d741eb14
chunChu <doanthanhchung03nb@gmail.com>
2024-01-09 17:22:25 +0700

    CORS Policy problem
    - npm i cors
    - import cors before using it

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,31 @@ import mongoose from "mongoose";
 import { Book } from "./model/bookModel.js";
 import { PORT, mongoDBURL } from "./config.js";
 import booksRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
+// var express = require("express");
+// var cors = require("cors");
 const app = express();
 //Middleware for parsing req body
 app.use(express.json());
+
+//middleware for handling cors policy
+//option 1 : allow all orgins with default cors
+// app.use(cors());
+//option 2 : Allow custom origin
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    method: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeader: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome");
 });
+
 //express route to manage book route
 app.use("/books", booksRoute);
 
